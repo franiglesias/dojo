aa69db512830790c576fc37d4ddcd1fa53935774
franiglesias <fran.iglesias@holaluz.com>
2019-04-14 18:09:18 +0200

    Dni kata

--- src/Dni.php
+++ src/Dni.php
@@ -4,52 +4,17 @@ declare(strict_types=1);
 namespace Dojo;
 
 use DomainException;
-use InvalidArgumentException;
 
 class Dni
 {
-    private const VALID_DNI_PATTERN = '/^[XYZ\d]\d{7,7}[^UIOÑ\d]$/u';
-    private const CONTROL_LETTER_MAP = 'TRWAGMYFPDXBNJZSQVHLCKE';
-    private const NIE_INITIAL_LETTERS = ['X', 'Y', 'Z'];
-    private const NIE_INITIAL_REPLACEMENTS = ['0', '1', '2'];
-    private const DIVISOR = 23;
-
-    /** @var string */
-    private $dni;
+    private const DNI_LENGTH = 9;
 
     public function __construct(string $dni)
     {
-        $dni = strtoupper($dni);
-        $this->checkIsValidDni($dni);
-
-        $mod = $this->calculateModulus($dni);
-
-        $letter = substr($dni, -1);
-
-        if ($letter !== self::CONTROL_LETTER_MAP[ $mod ]) {
-            throw new InvalidArgumentException('Invalid dni');
+        if (!preg_match('/^[0-9XYZ]\d{7,7}[A-Z]$/', $dni)) {
+            throw new \InvalidArgumentException('Bad Symbols');
         }
 
-        $this->dni = $dni;
-    }
-
-    public function __toString() : string
-    {
-        return $this->dni;
-    }
-
-    private function checkIsValidDni(string $dni) : void
-    {
-        if (!preg_match(self::VALID_DNI_PATTERN, $dni)) {
-            throw new DomainException('Bad format');
-        }
-    }
-
-    private function calculateModulus(string $dni) : int
-    {
-        $numeric = substr($dni, 0, -1);
-        $number = (int) str_replace(self::NIE_INITIAL_LETTERS, self::NIE_INITIAL_REPLACEMENTS, $numeric);
-
-        return $number % self::DIVISOR;
+        throw new DomainException('Invalid DNI');
     }
 }
--- tests/Dojo/DniTest.php
+++ tests/Dojo/DniTest.php
@@ -5,80 +5,45 @@ namespace Tests\Dojo;
 
 use Dojo\Dni;
 use DomainException;
-use InvalidArgumentException;
+use \InvalidArgumentException;
 use PHPUnit\Framework\TestCase;
 
 class DniTest extends TestCase
 {
-    public function testShouldFailWhenDniLongerThanMaxLenght() : void
-    {
-        $this->expectException(DomainException::class);
-        $dni = new Dni('0123456789');
-    }
-
-    public function testShouldFailWhenDniShorterThanMinLenght() : void
-    {
-        $this->expectException(DomainException::class);
-        $dni = new Dni('01234567');
-    }
-
-    public function testShouldFailWhenDniEndsWithANumber() : void
-    {
-        $this->expectException(DomainException::class);
-        $dni = new Dni('012345678');
-    }
-
-    public function testShouldFailWhenDniEndsWithAnInvalidLetter() : void
-    {
-        $this->expectException(DomainException::class);
-        $dni = new Dni('01234567I');
-    }
-
-    public function testShouldFailWhenDniHasLettersInTheMiddle() : void
-    {
-        $this->expectException(DomainException::class);
-        $dni = new Dni('012AB567R');
-    }
-
-    public function testShouldFailWhenDniStartsWithALetterOtherThanXYZ() : void
-    {
-        $this->expectException(DomainException::class);
-        $dni = new Dni('A1234567R');
-    }
-
-    public function testShouldFailWhenInvalidDni() : void
+    public function testShouldNotHaveMoreThanMaxChars(): void
     {
         $this->expectException(InvalidArgumentException::class);
+        $dni = new Dni('1234567890');
+    }
+
+    public function testShouldNotHaveLessThanMinChars(): void
+    {
+        $this->expectException(InvalidArgumentException::class);
+        $dni = new Dni('12345678');
+    }
+
+    public function testShouldNotEndWithNumber(): void
+    {
+        $this->expectException(InvalidArgumentException::class);
+        $dni = new Dni('123456789');
+    }
+
+    public function testShouldHaveNoLettersInTheMiddle(): void
+    {
+        $this->expectException(InvalidArgumentException::class);
+        $dni = new Dni('1234J678X');
+    }
+
+    public function testShouldNotStartWithALetterExceptXYZ(): void
+    {
+        $this->expectException(InvalidArgumentException::class);
+        $dni = new Dni('T2345678X');
+    }
+
+    public function testShouldFailWithInvalidDNI(): void
+    {
+        $this->expectException(DomainException::class);
         $dni = new Dni('00000000S');
     }
 
-    public function testShouldConstructValidDNIEndingWithT() : void
-    {
-        $dni = new Dni('00000000T');
-        $this->assertEquals('00000000T', (string) $dni);
-    }
-
-    public function testShouldConstructValidDNIEndingWithR() : void
-    {
-        $dni = new Dni('00000001R');
-        $this->assertEquals('00000001R', (string) $dni);
-    }
-
-    public function testShouldConstructValidDNIEndingWithW() : void
-    {
-        $dni = new Dni('00000002W');
-        $this->assertEquals('00000002W', (string) $dni);
-    }
-
-    public function testShouldConstructValidNIEStartingWithX() : void
-    {
-        $dni = new Dni('Y0000000Z');
-        $this->assertEquals('Y0000000Z', (string) $dni);
-    }
-
-    public function testShouldConstructValidDNIWithLowerCaseLetter(): void
-    {
-        $dni = new Dni('00000002w');
-        $this->assertEquals('00000002W', (string) $dni);
-    }
 }
